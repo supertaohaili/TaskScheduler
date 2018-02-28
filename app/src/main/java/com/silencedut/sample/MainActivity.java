ce2cf154091d8c4039509d8b86db2c03968602fa
陶海理 <15220055862@163.com>
2018-02-28 14:40:27 +0800

    Initial commit

---
+++ app/src/main/java/com/silencedut/sample/MainActivity.java
@@ -0,0 +1,119 @@
+package com.silencedut.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+
+import com.silencedut.taskscheduler.Task;
+import com.silencedut.taskscheduler.TaskScheduler;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private static final String TAG ="MainActivity" ;
+    private Task<String> mDemoTask;
+    private long mStartMillis;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.start_task).setOnClickListener(this);
+        findViewById(R.id.cancel_task).setOnClickListener(this);
+        findViewById(R.id.timeout_task).setOnClickListener(this);
+
+
+        mDemoTask = new Task<String>() {
+
+            @Override
+            public String doInBackground()  {
+
+                mStartMillis = System.currentTimeMillis();
+                try {
+                    Thread.sleep(8000);
+                }catch (InterruptedException e) {
+                    Log.i(TAG,""+e);
+                }
+                Log.i(TAG,"withResultTask doInBackground, current thread is ? "+Thread.currentThread().getName());
+                return "休眠"+(System.currentTimeMillis() - mStartMillis)/1000+"秒";
+            }
+
+            @Override
+            public void onSuccess(String result) {
+                Log.i(TAG,"withResultTask onSuccess, current thread is main ? "
+                        +TaskScheduler.isMainThread()+", result : "+result);
+            }
+
+            @Override
+            public void onFail(Throwable throwable) {
+                super.onFail(throwable);
+                Log.i(TAG,"onFail： 休眠 "+(System.currentTimeMillis() - mStartMillis)/1000+"秒");
+            }
+
+            @Override
+            public void onCancel() {
+                super.onCancel();
+                Log.i(TAG,"onCancel： 休眠 "+(System.currentTimeMillis() - mStartMillis)/1000+"秒");
+            }
+        };
+    }
+
+    private void noResultTask() {
+        TaskScheduler.execute(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Log.i(TAG,"noResultTask current thread is main ? "+TaskScheduler.isMainThread());
+            }
+        });
+    }
+    
+    private void withResultTask() {
+        TaskScheduler.execute(mDemoTask);
+    }
+
+    /**
+     *  期望结果是休眠3秒，回调到onCancel,代表超时
+     */
+    private void timeOutTask() {
+        TaskScheduler.executeTimeOutTask(3000, mDemoTask);
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        TaskScheduler.cancelTask(mDemoTask);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.start_task:
+                Log.i(TAG,"startTask");
+
+                noResultTask();
+                withResultTask();
+                break;
+            case R.id.cancel_task:
+                Log.i(TAG,"cancelTask");
+                TaskScheduler.cancelTask(mDemoTask);
+                break;
+            case R.id.timeout_task:
+                Log.i(TAG,"timeOutTask");
+                timeOutTask();
+                break;
+            default:break;
+        }
+    }
+}
